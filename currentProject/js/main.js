9fb31bf9460500107e1355c5af21952a6d042af3
masud001 <madhnagar@gmail.com>
2018-06-08 16:15:26 +0600

    mobile menu and header search input field added

--- currentProject/js/main.js
+++ currentProject/js/main.js
@@ -1,21 +1,30 @@
 //document ready function
 $(document).ready(function(){
-	var masud = $(".masud").length;
-	console.log("length : "+ masud);
-	$(".masud").click(function(){
-		// $(this).addClass("img-overlay");
+	// mobile menu toggle function
+	$('#mobile-menu-toggle').click(function( e ){
+		e.preventDefault();
+		$('#wrapper').toggleClass('mobile-menu-dispaly');
 	});
+
+	// header search field display
+	$(".user-search i.fa-search").click(function( e ){
+		$('.search-field').toggleClass('serach-field-display');
+	});
+
 });
+
+
+
+
+
+
+
+
+
 
 // window resize function
 $(window).on("resize", function(){
-	var width = $(window).width();
-	var outWindowWidth = window.outerWidth;
-	var innerWindowWidth = window.innerWidth;
 
-	console.log("width : "+ width);
-	console.log("outWindowWidth : "+ outWindowWidth);
-	console.log("innerWindowWidth : "+ innerWindowWidth);
-
+	
 
 }).resize();
